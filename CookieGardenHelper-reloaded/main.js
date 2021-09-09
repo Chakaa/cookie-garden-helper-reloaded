deb4774e5554a80c6ace0292dd23af48416d1861
Chakaa <39492995+Chakaa@users.noreply.github.com>
2021-09-09 23:55:59 +0200

    removed commented part

--- CookieGardenHelper-reloaded/main.js
+++ CookieGardenHelper-reloaded/main.js
@@ -588,13 +588,3 @@ Game.registerMod("cookiegardenhelperreloaded",{
 		this.build();
 	},
 });
-
-/*
-if (Garden.isActive) {
-  Main.init();
-} else {
-  let msg = `You don't have a garden yet. This mod won't work without it!`;
-  console.log(msg);
-  UI.createWarning(msg);
-}
-*/
